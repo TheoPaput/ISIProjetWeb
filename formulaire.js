8c77ea0ea64b2cdbfa04e29d2ebd655e37fa1e04
TheoPaput <theo.paput@gmail.com>
2017-03-13 22:34:27 +0100

    Amelioration du script (securite et couleurs)

--- formulaire.js
+++ formulaire.js
@@ -1,7 +1,8 @@
 var nom_valid = false, prenom_valid = false, naissance_valid = false, pseudo_valid = false, mdp_valid = false, email_valid = false;
 var form_valid = false;
 
-var checkNom = function() {
+
+var checkNom = function() {	/*Vérifie la validité du Nom (soit pas de caractère spéciaux)*/
 	var texte = document.getElementById("nom").value;
 	var valid = true;
 
@@ -11,6 +12,8 @@ var checkNom = function() {
 			if( c < 224)
 				valid = false;	
 		}
+		if (c < 97 && c > 90)
+			valid = false;
 	}
 
 	nom_valid = valid;
@@ -23,7 +26,8 @@ var checkNom = function() {
 	updateButton();		
 }
 
-var checkPrenom = function() {
+
+var checkPrenom = function() {	/*Vérifie la validité du Prénom (Pas de caractères spéciaux)*/
 	var texte = document.getElementById("prenom").value;
 	var valid = true;
 
@@ -33,6 +37,8 @@ var checkPrenom = function() {
 			if( c < 224)
 				valid = false;	
 		}
+		if (c < 97 && c > 90)
+			valid = false;
 	}
 
 	prenom_valid = valid;	
@@ -46,6 +52,114 @@ var checkPrenom = function() {
 	
 }
 
+
+var checkNaissance = function() {	/*Vérifie la validité de la date de naissance (Respect de la forme imposée)*/
+	var texte = document.getElementById("naissance").value;
+	var valid = true;
+	
+	for(var i = 0; i < texte.length; i++){
+		var c = texte.charCodeAt(i);
+		if ( i == 2 || i == 5){
+			if ( c != 47)
+				valid = false;
+		}
+		else{
+			if ( c < 48 || c > 57)
+				valid = false;
+		}
+	}
+	if( texte.length > 10)
+		valid = false;
+	
+	naissance_valid = valid;
+	
+	if(!naissance_valid)
+		document.getElementById("naissance").style.color = "#FF0000";
+	else
+		document.getElementById("naissance").style.color = "#000000";
+
+	updateButton();
+	
+}
+
+
+var checkPseudo = function() {	/*Vérifie la validité du Pseudo (Entre 3 et 20 caractères)*/
+	var texte = document.getElementById("pseudo").value;
+	var valid = true;
+
+	for(var i = 0; i < texte.length; i++){
+		var c = texte.charCodeAt(i);
+		if (c < 48 || c > 122)
+			valid = false;
+	}
+	if ( texte.length < 3 || texte.length > 20)
+		valid = false;
+
+	pseudo_valid = valid;	
+
+	if(!pseudo_valid)
+		document.getElementById("pseudo").style.color = "#FF0000";
+	else
+		document.getElementById("pseudo").style.color = "#000000";
+
+	updateButton();	
+	
+}
+
+
+var checkMdp = function() {	/*Vérifie la validité du Mot de passe (entre 8 et 16 caractères)*/
+	var texte = document.getElementById("mdp").value;
+	var valid = true;
+
+	if ( texte.length < 8 || texte.length > 16)
+		valid = false;
+
+	mdp_valid = valid;	
+
+	if(!mdp_valid)
+		document.getElementById("mdp").style.color = "#FF0000";
+	else
+		document.getElementById("mdp").style.color = "#000000";
+
+	updateButton();	
+	
+}
+
+
+var checkEmail = function() {	/*Vérifie la validité du Mot de passe (entre 8 et 16 caractères)*/
+	var texte = document.getElementById("email").value;
+	var valid = false;
+	
+	for(var i = 0; i < texte.length; i++){
+		var c = texte.charCodeAt(i);
+		valid = valid || (c == 64);
+	}
+
+	if ( texte.length < 5 || texte.length > 30)
+		valid = false;
+
+	email_valid = valid;	
+
+	if(!email_valid)
+		document.getElementById("email").style.color = "#FF0000";
+	else
+		document.getElementById("email").style.color = "#000000";
+
+	updateButton();	
+	
+}
+
+
+
 var updateButton = function(){
 	form_valid = nom_valid && prenom_valid && naissance_valid && pseudo_valid && mdp_valid && email_valid;
+	
+	if (form_valid){
+		document.getElementById("email").style.color = "#0f6ac4";
+		document.getElementById("mdp").style.color = "#0f6ac4";
+		document.getElementById("pseudo").style.color = "#0f6ac4";
+		document.getElementById("nom").style.color = "#0f6ac4";
+		document.getElementById("prenom").style.color = "#0f6ac4";
+		document.getElementById("naissance").style.color = "#0f6ac4";
+	}
 }
