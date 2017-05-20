0827dbd4f0ac2f56cb04787b2d4073cfa80df320
TheoPaput <theo.paput@gmail.com>
2017-05-21 00:32:47 +0200

    Modification apparance chatBox

--- chat.js
+++ chat.js
@@ -1,7 +1,11 @@
-var chatbox;
 var haut;
+var flagOpen = false;
 
-chatbox = document.getElementById("chatBoxDiv");
+var chatbox = document.getElementById("chatBoxDiv");
+var chatboxhead = document.getElementById("enteteChat");
+var chatboxheadtxt = document.getElementById("chatBoxHead");
+
+chatboxhead.addEventListener("click", openorclose, false);
 
 haut = (window.innerHeight);
 chatbox.style.top = (haut - 35) + "px";
@@ -9,5 +13,25 @@ chatbox.style.top = (haut - 35) + "px";
 window.onresize = function ()
 {
 	haut = (window.innerHeight);
-	chatbox.style.top = (haut - 35) + "px";
+	if (flagOpen == false){
+		chatbox.style.top = (haut - 35) + "px";
+	}
+	else{
+		chatbox.style.top = (haut - 600) + "px";
+	}
+}
+
+
+function openorclose(){
+	console.log("ttt");
+	if (flagOpen == false){
+		chatbox.style.top = (haut - 600) + "px";
+		chatboxheadtxt.innerHTML = "Clique ici pour fermer le chat";
+		flagOpen = true;
+	}
+	else{
+		chatbox.style.top = (haut - 35) + "px";
+		chatboxheadtxt.innerHTML = "Clique ici pour ouvrir le chat";
+		flagOpen = false;
+	}
 }
